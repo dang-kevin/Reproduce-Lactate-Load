4a5cce7e657a2d365bd68ac4dbcb72278948e459
Kevin Dang <dang.kevin@outlook.com>
2022-07-26 14:24:08 -0400

    Update cohort.sql

--- acquire/cohort/cohort.sql
+++ acquire/cohort/cohort.sql
@@ -9,7 +9,9 @@ WITH lactate AS (
     FROM `physionet-data.mimic_hosp.d_labitems` 
     WHERE label = 'Lactate'
   )  
-    AND DATE_DIFF(lab.charttime, icu.intime, HOUR) BETWEEN 0 AND 24
+    -- AND DATE_DIFF(lab.charttime, icu.intime, HOUR) BETWEEN 0 AND 24 -- inclusive but we need exclusive
+    AND DATE_DIFF(lab.charttime, icu.intime, HOUR) > 0
+    AND DATE_DIFF(lab.charttime, icu.intime, HOUR) < 24
   GROUP BY icu.stay_id
 )
 
@@ -18,7 +20,7 @@ SELECT icu.subject_id, icu.hadm_id, icu.stay_id
 , CASE WHEN age.anchor_age < 18 THEN 1 ELSE 0 END AS exclusion_non_adult
 , CASE WHEN ROW_NUMBER() OVER (PARTITION BY icu.subject_id ORDER BY icu.intime) > 1 THEN 1 ELSE 0 END AS exclusion_readmission
 , CASE WHEN lac.lactate_measurement_count = 1 THEN 1 ELSE 0 END AS exclusion_one_lactate_measurement
-, CASE WHEN DATE_DIFF(outtime, intime, HOUR) <= 4 THEN 1 ELSE 0 END AS exclusion_short_icu_stay
+, CASE WHEN DATE_DIFF(outtime, intime, HOUR) < 24 THEN 1 ELSE 0 END AS exclusion_short_icu_stay
 , CASE WHEN sep.sepsis3 = TRUE THEN 1 ELSE 0 END AS sepsis_flag 
 FROM `physionet-data.mimic_icu.icustays` AS icu
 INNER JOIN `physionet-data.mimic_derived.age` AS age
@@ -27,3 +29,4 @@ INNER JOIN lactate AS lac
   ON icu.stay_id = lac.stay_id
 INNER JOIN `physionet-data.mimic_derived.sepsis3` AS sep
   ON icu.stay_id = sep.stay_id
+ORDER BY subject_id, hadm_id, stay_id
