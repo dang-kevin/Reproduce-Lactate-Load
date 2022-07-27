7d55a860824a2a10e8491623fc6e810204e533ed
Kevin Dang <dang.kevin@outlook.com>
2022-07-26 22:49:59 -0400

    Update comorbidities.sql

--- acquire/covariates/comorbidities.sql
+++ acquire/covariates/comorbidities.sql
@@ -6,25 +6,25 @@ WITH con_hf AS (
 )
 
 , myo_inf AS (
-  SELECT * 
+  SELECT icd_code 
   FROM `physionet-data.mimic_hosp.d_icd_diagnoses` 
   WHERE LOWER(long_title) LIKE '%myocardial infarction%'
 )
 
 , cerebro AS (
-  SELECT * 
+  SELECT icd_code 
   FROM `physionet-data.mimic_hosp.d_icd_diagnoses` 
   WHERE LOWER(long_title) LIKE '%cerebrovascular disease%'
 )
 
 , chron_pulm AS (
-  SELECT * 
+  SELECT icd_code 
   FROM `physionet-data.mimic_hosp.d_icd_diagnoses` 
   WHERE LOWER(long_title) LIKE '%chronic pulmonary%'
 )
 
 , liver AS (
-  SELECT * 
+  SELECT icd_code 
   FROM `physionet-data.mimic_hosp.d_icd_diagnoses` 
   WHERE LOWER(long_title) LIKE '%liver disease%'
 )
@@ -45,20 +45,21 @@ WITH con_hf AS (
 )
 
 , renal AS (
-  SELECT * 
+  SELECT icd_code 
   FROM `physionet-data.mimic_hosp.d_icd_diagnoses` 
   WHERE LOWER(long_title) LIKE '%renal disease%' 
 )
 
 
 SELECT DISTINCT subject_id, hadm_id
-, CASE WHEN icd_code IN (SELECT icd_code FROM con_hf) THEN 1 ELSE 0 END AS congestive_heart_failure
-, CASE WHEN icd_code IN (SELECT icd_code FROM myo_inf) THEN 1 ELSE 0 END AS myocardial_infarction
-, CASE WHEN icd_code IN (SELECT icd_code FROM cerebro) THEN 1 ELSE 0 END AS cerebrovascular_disease 
-, CASE WHEN icd_code IN (SELECT icd_code FROM chron_pulm) THEN 1 ELSE 0 END AS chronic_pulmonary_disease
-, CASE WHEN icd_code IN (SELECT icd_code FROM liver) THEN 1 ELSE 0 END AS liver_disease  
-, CASE WHEN icd_code IN (SELECT icd_code FROM diab_wo_comp) THEN 1 ELSE 0 END AS diabetes_without_complication
-, CASE WHEN icd_code IN (SELECT icd_code FROM diab_w_comp) THEN 1 ELSE 0 END AS diabetes_with_complication
-, CASE WHEN icd_code IN (SELECT icd_code FROM renal) THEN 1 ELSE 0 END AS renal_disease  
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM con_hf) THEN 1 ELSE 0 END) AS congestive_heart_failure
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM myo_inf) THEN 1 ELSE 0 END) AS myocardial_infarction
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM cerebro) THEN 1 ELSE 0 END) AS cerebrovascular_disease 
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM chron_pulm) THEN 1 ELSE 0 END) AS chronic_pulmonary_disease
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM liver) THEN 1 ELSE 0 END) AS liver_disease  
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM diab_wo_comp) THEN 1 ELSE 0 END) AS diabetes_without_complication
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM diab_w_comp) THEN 1 ELSE 0 END) AS diabetes_with_complication
+, MAX(CASE WHEN icd_code IN (SELECT icd_code FROM renal) THEN 1 ELSE 0 END) AS renal_disease  
 FROM `physionet-data.mimic_hosp.diagnoses_icd`
+GROUP BY subject_id, hadm_id
 ORDER BY subject_id, hadm_id
